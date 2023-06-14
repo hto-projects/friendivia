c1d455326dee951845b3aee9217efe6779de258f
Tyler Bordeaux <tylerbordeaux2025@gmail.com>
2023-06-14 12:03:50 -0400

    fixed typo

--- front-end/src/host/HostQuestionnaire.tsx
+++ front-end/src/host/HostQuestionnaire.tsx
@@ -1,23 +1,25 @@
-import * as React from 'react';
-import '../style.css';
-import { Button } from '@mui/material';
-import { Socket } from 'socket.io-client';
+import * as React from "react";
+import "../style.css";
+import { Button } from "@mui/material";
+import { Socket } from "socket.io-client";
 
 interface IHostQuestionnaireProps {
-  socket: Socket
+  socket: Socket;
 }
 
 export default function HostQuestionnaire(props: IHostQuestionnaireProps) {
   const { socket } = props;
 
   async function onDoThis() {
-    socket.emit('nothing');
+    socket.emit("nothing");
   }
 
   return (
     <>
-      <p>We are waiting for everyone to compete the questionnaire.</p>
-      <Button variant="contained" onClick={onDoThis}>Do This</Button>
+      <p>We are waiting for everyone to complete the questionnaire.</p>
+      <Button variant="contained" onClick={onDoThis}>
+        Do This
+      </Button>
     </>
   );
 }
