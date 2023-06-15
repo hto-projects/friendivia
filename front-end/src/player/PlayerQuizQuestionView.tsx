fd090b70b0cb9bf1fbb7d3c5748cd32e54fbf529
Tyler Bordeaux <tylerbordeaux2025@gmail.com>
2023-06-15 11:55:27 -0400

    Styled answer view on users screens for mobile

--- front-end/src/player/PlayerQuizQuestionView.tsx
+++ front-end/src/player/PlayerQuizQuestionView.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import '../style.css';
-import { Button } from '@mui/material';
-import { Socket } from 'socket.io-client';
-import PlayerWait from './PlayerWait';
+import * as React from "react";
+import "../style.css";
+import { Button } from "@mui/material";
+import { Socket } from "socket.io-client";
+import PlayerWait from "./PlayerWait";
 
 interface IQuizQuestionViewProps {
-  optionsList: string[],
-  socket: Socket,
+  optionsList: string[];
+  socket: Socket;
   playerState: {
-    state: string,
-    message: string
-  }
+    state: string;
+    message: string;
+  };
 }
 
 export default function PlayerQuizQuestionView(props: IQuizQuestionViewProps) {
@@ -19,20 +19,33 @@ export default function PlayerQuizQuestionView(props: IQuizQuestionViewProps) {
   const guessReceivedMessage = `Guess received! Hang tight...`;
 
   const answerQuestion = async (answerIndex: number): Promise<void> => {
-    socket.emit('player-answer-question', answerIndex);
-  }
+    socket.emit("player-answer-question", answerIndex);
+  };
 
   const optionsForm = (
     <div>
-      <h4>Select your answer below</h4>
-      {optionsList.map((o: String, i: number) => (
-        <Button key={i} onClick={() => answerQuestion(i)}>{o}</Button>
-      ))}
-      <p style={{color: 'red'}}>{playerState.message}</p>
+      <div className="answerOptions">
+        {optionsList.map((o: String, i: number) => (
+          <>
+            <br />
+            <Button
+              className="answerButton"
+              variant="contained"
+              sx={{ bgcolor: "#757de8;" }}
+              key={i}
+              onClick={() => answerQuestion(i)}
+            >
+              {o}
+            </Button>
+          </>
+        ))}
+      </div>
+
+      <p style={{ color: "red" }}>{playerState.message}</p>
     </div>
   );
 
-  if (playerState.state === 'answered-quiz-question-waiting') {
+  if (playerState.state === "answered-quiz-question-waiting") {
     return <PlayerWait message={guessReceivedMessage} />;
   } else {
     return optionsForm;
