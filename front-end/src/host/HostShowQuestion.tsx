6a100ecb1b570634c5dfdd3b73c89fdedac79546
Tyler Bordeaux <tylerbordeaux2025@gmail.com>
2023-06-15 11:54:40 -0400

    Styled host question view

--- front-end/src/host/HostShowQuestion.tsx
+++ front-end/src/host/HostShowQuestion.tsx
@@ -1,10 +1,11 @@
-import * as React from 'react';
-import '../style.css';
+import * as React from "react";
+import "../style.css";
+import { Button, Paper } from "@mui/material";
 
 interface IShowQuestionProps {
-  playerName: string,
-  questionText: string,
-  options: string[]
+  playerName: string;
+  questionText: string;
+  options: string[];
 }
 
 export default function HostShowQuestion(props: IShowQuestionProps) {
@@ -12,16 +13,26 @@ export default function HostShowQuestion(props: IShowQuestionProps) {
 
   function interpolatePlayerNameInQuestionText() {
     const [part1, part2] = questionText.split("<PLAYER>");
-    return <p>{part1}<b>{playerName}</b>{part2}</p>;
+    return (
+      <p>
+        {part1}
+        <b>{playerName}</b>
+        {part2}
+      </p>
+    );
   }
 
   return (
     <>
       {interpolatePlayerNameInQuestionText()}
-      <ul>
-      {options.map((o: String, i: number) => (
-        <li key={i}>{o}</li>
-      ))}
+      <ul className="ul">
+        {options.map((o: String, i: number) => (
+          <Paper elevation={3} className="paper">
+            <li className="answer" key={i}>
+              {o}
+            </li>
+          </Paper>
+        ))}
       </ul>
     </>
   );
