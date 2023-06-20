68708b2e3a188222042d8be1d0b67486ce118859
Andrew <matttoodeetoo@gmail.com>
2023-06-20 12:48:50 -0400

    Fixed Alignment error

--- front-end/src/player/PlayerApp.tsx
+++ front-end/src/player/PlayerApp.tsx
@@ -19,13 +19,8 @@ export default function PlayerApp(props: PlayerAppProps) {
   const playerIdFromStorage = localStorage.getItem("player-id") || "";
   const [playerState, setPlayerState] = React.useState("");
   const [playerName, setPlayerName] = React.useState("");
-  const [
-    questionnaireQuestionsText,
-    setQuestionnaireQuestionsText,
-  ] = React.useState<string[]>([]);
-  const [quizQuestionOptionsText, setQuizQuestionOptionsText] = React.useState<
-    string[]
-  >([]);
+  const [questionnaireQuestionsText, setQuestionnaireQuestionsText] = React.useState<string[]>([]);
+  const [quizQuestionOptionsText, setQuizQuestionOptionsText] = React.useState<string[]>([]);
   const [loaded, setLoaded] = React.useState<boolean>(false);
 
   const { socket } = props;
