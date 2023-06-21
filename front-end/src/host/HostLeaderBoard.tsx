8740bb65340a4482110a1ac0593451b6a01929d5
Tyler Bordeaux <tylerbordeaux2025@gmail.com>
2023-06-21 13:14:48 -0400

    Fixed sorting

--- front-end/src/host/HostLeaderBoard.tsx
+++ front-end/src/host/HostLeaderBoard.tsx
@@ -8,14 +8,16 @@ interface ILeaderBoardProps {
 export default function HostLeaderBoard(props: ILeaderBoardProps) {
   const playerScores = props.playerScores;
 
-  playerScores.sort((p1, p2) => p1.score - p2.score);
+  playerScores.sort((p1, p2) => p2.score - p1.score);
 
   return (
     <>
-      <h1>Leader board</h1>
+      <h1>Leaderboard</h1>
       <ol>
         {playerScores.map((ps, i) => (
-          <li key={i}>{ps.name}: {ps.score}</li>
+          <li key={i}>
+            {ps.name}: {ps.score}
+          </li>
         ))}
       </ol>
     </>
