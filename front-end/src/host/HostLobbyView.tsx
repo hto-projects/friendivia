3563bb11aa9abfbea1eb412c8afbc34e81a2a002
Tyler Bordeaux <tylerbordeaux2025@gmail.com>
2023-07-11 14:52:58 -0400

    Can fit more players on lobby screen

--- front-end/src/host/HostLobbyView.tsx
+++ front-end/src/host/HostLobbyView.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
+import React from "react";
 import "../style.css";
-import { Button, Paper } from "@mui/material";
+import { Button, Paper, Grid } from "@mui/material";
 import { Socket } from "socket.io-client";
 
 interface ILobbyViewProps {
@@ -27,33 +27,31 @@ export default function HostLobbyView(props: ILobbyViewProps) {
         <p className="id">{gameId}</p>
       </Paper>
       <h1>{playerNames.length} Players</h1>
-      <ul className="ul">
+      <div className="player-list">
         {playerNames.map((name: string, i: number) => (
-          <li key={i}>
-            <Paper
-              elevation={3}
-              sx={{
-                "&:hover": {
-                  cursor: "pointer",
-                  boxShadow: 8,
-                  textDecoration: "strike-through",
-                },
-              }}
-              className="lobby_player"
-              onClick={() => onPlayerKick(name)}
-            >
-              <p className="player">{name}</p>
-            </Paper>
-            <br />
-          </li>
+          <Paper
+            key={i}
+            elevation={3}
+            className="lobby_player"
+            sx={{
+              "&:hover": {
+                cursor: "pointer",
+                boxShadow: 8,
+                textDecoration: "line-through",
+              },
+            }}
+            onClick={() => onPlayerKick(name)}
+          >
+            <p className="player">{name}</p>
+          </Paper>
         ))}
-      </ul>
+      </div>
       <Button
         variant="contained"
         disabled={playerNames.length < 2}
+        style={{ marginTop: "5vh" }}
         sx={{
-          bgcolor:
-            getComputedStyle(document.body).getPropertyValue("--accent") + ";",
+          bgcolor: getComputedStyle(document.body).getPropertyValue("--accent"),
         }}
         onClick={onStart}
       >
