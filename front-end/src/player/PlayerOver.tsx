cb58bcdfd9d4f2e499b8e37787b3739e0dde66d0
Tyler Bordeaux <tylerbordeaux2025@gmail.com>
2023-07-13 10:03:59 -0400

    Screens for 1st, 2nd, third

--- front-end/src/player/PlayerOver.tsx
+++ front-end/src/player/PlayerOver.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import "../style.css";
 import correct from "../assets/correct.png";
 import incorrect from "../assets/incorrect.png";
+import rankone from "../assets/rankone.png";
+import ranktwo from "../assets/ranktwo.png";
+import rankthree from "../assets/rankthree.png";
 
 interface IOverProps {
   rank: number;
@@ -10,14 +13,28 @@ interface IOverProps {
 export default function PlayerOver(props: IOverProps) {
   return (
     <>
-      <div className={props.rank === 1 ? "correct" : "incorrect"}>
+      <div
+        className={
+          props.rank === 1 || props.rank === 2 || props.rank === 3
+            ? "correct"
+            : "incorrect"
+        }
+      >
         {props.rank === 1 ? (
-          <img className="correctImg" src={correct} />
+          <img className="correctImg" src={rankone} />
+        ) : props.rank === 2 ? (
+          <img className="correctImg" src={ranktwo} />
+        ) : props.rank === 3 ? (
+          <img className="correctImg" src={rankthree} />
         ) : (
           <img className="correctImg" src={incorrect} />
         )}
         {props.rank === 1 ? (
           <p className="correctTxt">You won!</p>
+        ) : props.rank === 2 ? (
+          <p className="correctTxt">Second place!</p>
+        ) : props.rank === 3 ? (
+          <p className="correctTxt">Third place!</p>
         ) : (
           <p className="correctTxt">You lost!</p>
         )}
