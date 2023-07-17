fa250dbf24bc992a5489a5aeba2f31edf0917b62
Joseph Maxwell <joseph.maxwell@hyland.com>
2023-07-17 12:38:23 -0400

    add some basic audio capability

--- front-end/src/host/HostQuestionnaire.tsx
+++ front-end/src/host/HostQuestionnaire.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import '../style.css';
+import PlayAudio from '../PlayAudio';
+import glitch from "../assets/audio/glitch.mp3";
 
 export default function HostQuestionnaire() {
-  return <p>We are waiting for everyone to complete the questionnaire.</p>;
+  return (
+    <>
+      <PlayAudio src={glitch} />
+      <p>We are waiting for everyone to complete the questionnaire.</p>
+    </>
+  );
 }
