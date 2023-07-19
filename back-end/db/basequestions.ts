1b5ef3de5571a5cd36af6d27f88700532b22dd60
Andrew Chow <Arrowship06@gmail.com>
2023-07-19 12:48:16 -0400

    Added one answer to each question

--- back-end/db/basequestions.ts
+++ back-end/db/basequestions.ts
@@ -2,122 +2,122 @@ const arr = [
     {
         text: "What is your favorite movie?",
         quizText: "What is <PLAYER>'s favorite movie?",
-        fakeAnswers: ["The Godfather", "Despicable Me", "Into the Spiderverse"]
+        fakeAnswers: ["The Godfather", "Despicable Me", "Into the Spiderverse", "Star Wars: A New Hope"]
     },
     {
         text: "What do you do for fun?",
         quizText: "What does <PLAYER> do for fun?",
-        fakeAnswers: ["Nothing", "watch tv", "play music"]
+        fakeAnswers: ["Nothing", "watch tv", "play music", "Minecraft"]
     },
     {
         text: "Where were you born?",
         quizText: "Where was <PLAYER> born?",
-        fakeAnswers: ["Ohio", "on Planet Earth", "Detroit, Michigan"]
+        fakeAnswers: ["Ohio", "on Planet Earth", "Detroit, Michigan", "New York"]
     },
     {
         text: "What is your favorite animal?",
         quizText: "What is <PLAYER>'s favorite animal?",
-        fakeAnswers: ["dog", "Tiger", "Grey Wolf"]
+        fakeAnswers: ["dog", "Tiger", "Grey Wolf", "Felis catus"]
     },
     {
         text: "How many sibling(s) do you have?",
         quizText: "How many sibling(s) does <PLAYER> have?",
-        fakeAnswers: ["0", "none", "2"]
+        fakeAnswers: ["0", "none", "2", "9"]
     },
     {
         text: "What's your greatest fear?",
         quizText: "What's <PLAYER>'s greatest fear?",
-        fakeAnswers: ["spiders", "no fears", "Worms"]
+        fakeAnswers: ["spiders", "no fears", "Worms", "DEATH"]
     },
     {
         text: "What's your favorite food?",
         quizText: "What's <PLAYER>'s favorite food?",
-        fakeAnswers: ["pizza", "Pasta", "sushi"]
+        fakeAnswers: ["pizza", "Pasta", "sushi", "Taco"]
     },
     {
         text: "What's your favorite day of the week?",
         quizText: "What's <PLAYER>'s favorite day of the week?",
-        fakeAnswers: ["monday", "thursday", "Sunday"]
+        fakeAnswers: ["monday", "thursday", "Sunday", "Today"]
     },
     {
         text: "What is your birthday month?",
         quizText: "What is <PLAYER>'s birthday month?",
-        fakeAnswers: ["March", "november", "June"]
+        fakeAnswers: ["March", "november", "June", "December"]
     },
     {
         text: "What is your zodiac sign?",
         quizText: "What is <PLAYER>'s zodiac sign?",
-        fakeAnswers: ["sagittarius", "Libra", "leo"]
+        fakeAnswers: ["sagittarius", "Libra", "leo", "Cancer"]
     },
     {
         text: "What is your favorite season?",
         quizText: "What is <PLAYER>'s favorite season?",
-        fakeAnswers: ["Spring!!", "summer", "snowy winters"]
+        fakeAnswers: ["Spring!!", "summer", "snowy winters", "fall"]
     },
     {
         text: "What is your favorite color?",
         quizText: "What is <PLAYER>'s favorite color?",
-        fakeAnswers: ["Blue", "turquoise", "crimson"]
+        fakeAnswers: ["Blue", "turquoise", "crimson", "Rainbow"]
     },
     {
         text: "If you could go anywhere in the world, where would you go?",
         quizText: "If <PLAYER> could go anywhere in the world, where would they go?",
-        fakeAnswers: ["Eiffel Tower", "dubai", "Japan"]
+        fakeAnswers: ["Eiffel Tower", "dubai", "Japan", "Home"]
     },
     {
-        text: "Who is your favorite singer?",
-        quizText: "Who is <PLAYER>'s favorite singer?",
-        fakeAnswers: ["harry styles", "Taylor Swift", "weeknd"]
+        text: "Who is your favorite musical artist?",
+        quizText: "Who is <PLAYER>'s favorite musical artist?",
+        fakeAnswers: ["harry styles", "Taylor Swift", "weeknd", "Journey"]
     },
     {
         text: "What is your favorite ice cream flavor?",
         quizText: "What is <PLAYER>'s favorite ice cream flavor?",
-        fakeAnswers: ["chocolate fudge brownie", "Strawberry", "raspberry"]
+        fakeAnswers: ["chocolate fudge brownie", "Strawberry", "raspberry", "VaNilla"]
     },
     {
         text: "If you can spend $5000 only at one place, where would you spend it?",
         quizText: "If <PLAYER> can spend $5000 only at one place, where would they spend it?",
-        fakeAnswers: ["kohl's", "target", "amazon"]
+        fakeAnswers: ["kohl's", "target", "amazon", "Apple Store"]
     },
     {
         text: "What is one thing you are allergic to?",
         quizText: "What is one thing <PLAYER> is allergic to?",
-        fakeAnswers: ["dust", "pollen", "latex"]
+        fakeAnswers: ["dust", "pollen", "latex", "mulberries"]
     },
     {
         text: "How many languages can you speak?",
         quizText: "How many languages can <PLAYER> speak?",
-        fakeAnswers: ["zErO", "5", "two"]
+        fakeAnswers: ["zErO", "5", "two", "Hablo dos"]
     },
     {
         text: "If you could have any one superpower, what would it be?",
         quizText: "If <PLAYER> could have any one superpower, what would it be?",
-        fakeAnswers: ["invisibility", "FLYYYY", "teleportation"]
+        fakeAnswers: ["invisibility", "FLYYYY", "teleportation", "omniscient"]
     },
     {
         text: "What is your favorite book genre?",
         quizText: "What is <PLAYER>'s favorite book genre?",
-        fakeAnswers: ["fantasy", "thriller", "Romance"]
+        fakeAnswers: ["fantasy", "thriller", "Romance", "historical-fiction"]
     },
     {
         text: "If there were no consequences, what law would you break?",
         quizText: "If there were no consequences, what law would <PLAYER> break?",
-        fakeAnswers: ["NO TAXES", "Share netflix password", "jaywalking"]
+        fakeAnswers: ["NO TAXES", "Share netflix password", "jaywalking",]
     },
     {
         text: "What is the name of your best friend?",
         quizText: "What is the name of <PLAYER>'s best friend?",
-        fakeAnswers: ["Laura", "Jonathan", "Emmanuel"]
+        fakeAnswers: ["Laura", "Jonathan", "Emmanuel", "Eliza"]
     },
     {
         text: "If you could remain the same age forever, what would it be?",
         quizText: "If <PLAYER> could remain the same age forever, what would it be?",
-        fakeAnswers: ["8", "25", "two"]
+        fakeAnswers: ["8", "25", "two", "21"]
     },
     {
         text: "What did you dream of becoming when you were little?",
         quizText: "What did <PLAYER> dream of becoming when they were little?",
-        fakeAnswers: ["truck driver", "gangster", "royalty"]
+        fakeAnswers: ["truck driver", "gangster", "royalty", "Astronaut"]
     }
     ];
 
