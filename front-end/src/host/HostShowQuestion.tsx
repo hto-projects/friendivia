a4602a7cf93864173113c6337dbebe3056906cc5
Ayushi Mukherjee <tam15@bereaschools.org>
2023-07-20 09:58:53 -0400

    banner and host lobby background changed

--- front-end/src/host/HostShowQuestion.tsx
+++ front-end/src/host/HostShowQuestion.tsx
@@ -23,7 +23,7 @@ export default function HostShowQuestion(props: IShowQuestionProps) {
     return (
       <div className="dot">
         <div className="timer">
-          <div>{counter}</div>
+          <div className="timeNumber">{counter}</div>
         </div>
       </div>
     );
