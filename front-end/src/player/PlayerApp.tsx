513a84157f0f2ecf57058e59e811cd41639b2dcd
Andrew <102868134+Arrowship06@users.noreply.github.com>
2023-07-25 13:31:10 -0400

    remove comments PlayerApp.tsx

--- front-end/src/player/PlayerApp.tsx
+++ front-end/src/player/PlayerApp.tsx
@@ -62,10 +62,6 @@ export default function PlayerApp(props: PlayerAppProps) {
       if (data && data.extraData && data.extraData.quizQuestionOptionsText) {
         setQuizQuestionOptionsText(data.extraData.quizQuestionOptionsText);
       }
-
-      /*if (data && data.extraData && data.extraData.playerScores){
-        setAllPlayerScores(data.extraData.playerScores);
-      }*/
     }
 
     socket.on("player-load-success", onLoadSuccess);
