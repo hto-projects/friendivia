60972759d0f3890bdcf18d8cbc2631935b158270
Andrew Chow <Arrowship06@gmail.com>
2023-07-26 13:54:15 -0400

    Switch mute to use react to set local

--- front-end/src/host/HostApp.tsx
+++ front-end/src/host/HostApp.tsx
@@ -60,7 +60,7 @@ export default function HostApp(props: IHostProps) {
   const { socket } = props;
 
   function muteMusic(muted: boolean) {
-    setMuted(localStorage.getItem('Music-Playing') !== 'true' ? true : false);
+    setMuted(!muted)
     localStorage.setItem('Music-Playing', muted.toString());
 
     if (muted) {
