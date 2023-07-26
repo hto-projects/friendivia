c95408cadbec853c39304e42b9de0104173aebdf
matandon <manyatandon11@gmail.com>
2023-07-26 10:39:02 -0400

    quick syntax fix - not related to current issue

--- back-end/db/utils.ts
+++ back-end/db/utils.ts
@@ -28,7 +28,7 @@ const chooseRandomFromList = (listOfSomething: any[]): any => {
 
 const selectRandom = (mainList, newList, count) => {
     let mainCopy = [...mainList];
-    let newList_lower = newList.map(newList => newList_item.toLowerCase());
+    let newList_lower = newList.map(newList_item => newList_item.toLowerCase());
     while (newList.length < count) {
       let newValue = chooseRandomFromList(mainCopy);
       if (!newList_lower.includes(newValue.toLowerCase())) {
