ea6af4ede3f88aa1eda3f421a3e8cb47fed365f3
Tyler Bordeaux <tylerbordeaux2025@gmail.com>
2023-08-07 21:36:30 -0400

    Revert "more brand updates"
    This reverts commit f688fe293531d9e53c14a7281d07dce9123c8af8.

--- front-end/src/host/HostPreQuiz.tsx
+++ front-end/src/host/HostPreQuiz.tsx
@@ -5,18 +5,9 @@ import Speak from "../Speak";
 export default function HostPreQuiz() {
   return (
     <>
-      <Speak
-        text="All questionnaires have been completed. Party time is imminent."
-        cloud={true}
-      />
-      <p style={{ fontSize: "1.5em" }}>
-        All questionnaires have been completed! The quiz will begin soon...
-      </p>
+      <Speak text="All questionnaires have been completed. It's time to start the quiz." cloud={true} />
+      <p style={{fontSize: "1.5em"}}>All questionnaires have been completed! It's time to start the quiz.</p>
     </>
   );
-  return (
-    <p style={{ fontSize: "1.5em" }}>
-      All questionnaires have been completed! The quiz will begin soon...
-    </p>
-  );
+  return <p style={{fontSize: "1.5em"}}>All questionnaires have been completed! The quiz will begin soon...</p>;
 }
